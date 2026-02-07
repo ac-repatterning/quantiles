6977e14ab2f508335b5fcd0948144a7377546fde
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-07 19:54:35 +0000

    Update src/algorithms/quantiles.py: In Progress

--- src/algorithms/quantiles.py
+++ src/algorithms/quantiles.py
@@ -1,6 +1,7 @@
 """Module quantiles.py"""
 
 import cudf
+import numpy as np
 
 
 class Quantiles:
@@ -12,10 +13,28 @@ class Quantiles:
         pass
 
     @staticmethod
-    def exc(data: cudf.DataFrame) -> cudf.DataFrame:
+    def __get_aggregates(data: cudf.DataFrame):
+
+        __metrics = data['measure'].quantile(q = [0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95])
+        metrics = __metrics.to_dict()
+
+        sequence = data['measure'].to_numpy()
+        i_minimum = np.nanargmin(sequence)
+        i_maximum = np.nanargmax(sequence)
+
+        metrics.update({
+            'minimum': float(data['measure'].values[i_minimum]),
+            'minimum_': float(data['timestamp'].values[i_minimum]),
+            'maximum': float(data['measure'].values[i_maximum]),
+            'maximum_': float(data['timestamp'].values[i_maximum])
+        })
+
+        return metrics
+
+    def exc(self, data: cudf.DataFrame) -> cudf.DataFrame:
         """
 
-        :param data: A data frame consisting of fields ['date', 'measure'] <b>only</b>.<br>
+        :param data: A data frame consisting of fields ['date', 'timestamp', 'measure'] <b>only</b>.<br>
         :return:
         """
 
@@ -24,6 +43,10 @@ class Quantiles:
         q050 = lambda z: z.quantile(0.50); q050.__name__ = 'median'
         q075 = lambda z: z.quantile(0.75); q075.__name__ = 'u_quartile'
         q090 = lambda z: z.quantile(0.90); q090.__name__ = 'u_whisker'
-        x = data.groupby(by='date', as_index=True, axis=0).agg([q010, q025, q050, q075, q090, 'min', 'max'])
+        frame = data[['date', 'measure']].groupby(by='date', as_index=True, axis=0).agg(
+            [q010, q025, q050, q075, q090, 'min', 'max'])
 
-        return x.loc[:, 'measure']
+        aggregates = self.__get_aggregates(data=data)
+        print(aggregates)
+
+        return frame.loc[:, 'measure']
