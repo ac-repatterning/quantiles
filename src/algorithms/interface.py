375e0b2ae5acdd48111dba9da9a6c17f3af0c195
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-08 08:39:11 +0000

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -18,24 +18,39 @@ class Interface:
     The interface to quantiles calculations.
     """
 
-    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters, arguments: dict):
+    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters, reference: pd.DataFrame, arguments: dict):
         """
 
         :param service: A suite of services for interacting with Amazon Web Services.
         :param s3_parameters: The overarching S3 parameters settings of this
                               project, e.g., region code name, buckets, etc.
+        :param reference: The reference sheet of gauges.  Each instance encodes the attributes of a gauge.
         :param arguments: A set of arguments vis-à-vis calculation & storage objectives.
         """
 
         self.__service = service
         self.__s3_parameters = s3_parameters
+        self.__reference = reference
         self.__arguments = arguments
 
-    def exc(self, partitions: list[prt.Partition], reference: pd.DataFrame):
+        # Names
+        self.__rename = {
+            0.05: 'e_l_whisker', 0.10: 'l_whisker', 0.25: 'l_quartile', 0.5: 'median',
+            0.75: 'u_quartile', 0.9: 'u_whisker', 0.95: 'e_u_whisker'}
+
+    def __persist(self, aggregates: dict):
+
+        frame = pd.DataFrame.from_dict(aggregates)
+        frame.rename(columns=self.__rename, inplace=True)
+        details = frame.merge(self.__reference, how='left', on=['catchment_id', 'ts_id'])
+
+        logging.info(details)
+
+    def exc(self, partitions: list[prt.Partition], ):
         """
 
         :param partitions: The time series partitions.
-        :param reference: The reference sheet of gauges.  Each instance encodes the attributes of a gauge.
+
         :return:
         """
 
@@ -43,9 +58,9 @@ class Interface:
         __data = dask.delayed(src.algorithms.data.Data(
             service=self.__service, s3_parameters=self.__s3_parameters, arguments=self.__arguments).exc)
         __metrics = dask.delayed(src.algorithms.metrics.Metrics(
-            reference=reference).exc)
+            reference=self.__reference).exc)
         __persist = dask.delayed(src.algorithms.persist.Persist(
-            reference=reference).exc)
+            reference=self.__reference).exc)
 
         computations = []
         for partition in partitions:
@@ -54,13 +69,3 @@ class Interface:
             computations.append(__aggregates)
         aggregates = dask.compute(computations, scheduler='threads')[0]
         logging.info(aggregates)
-
-        frame = pd.DataFrame.from_dict(aggregates)
-        frame.rename(columns={
-            0.05: 'e_l_whisker', 0.10: 'l_whisker', 0.25: 'l_quartile', 0.5: 'median',
-            0.75: 'u_quartile', 0.9: 'u_whisker', 0.95: 'e_u_whisker'},
-            inplace=True)
-        logging.info(frame)
-
-        details = frame.merge(reference, how='left', on=['catchment_id', 'ts_id'])
-        logging.info(details)
